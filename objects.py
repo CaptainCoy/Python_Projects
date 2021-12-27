98c5f5265e918203e2d84d6a2d60919d61c32b6c
Alex Coy <88282738+CaptainCoy@users.noreply.github.com>
2021-12-27 15:19:24 -0700

    Add files via upload

---
+++ objects.py
@@ -0,0 +1,14 @@
+class Player:
+    player = input('Name: ')
+    password = input('Password: ')
+    lives = 3
+
+class Mario(Player):
+    player_1 = True
+    color = 'red'
+
+class Luigi(Player):
+    player_2 = True
+    color = 'green'
+    
+    
